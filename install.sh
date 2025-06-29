8ecafd5c6eed988d102080eb59198814100381ae
Arc360 <155182753+arc360alt@users.noreply.github.com>
2025-06-29 00:11:58 -0500

    Linux EasyInstall scripts

---
+++ install.sh
@@ -0,0 +1,24 @@
+echo "-------------------------------------"
+echo "   ArkGPT Easy Install Script 0.1"
+echo "-------------------------------------"
+echo "installing Ollama if you have not already"
+
+echo "If you have already installed Ollama press ctrl+c to quit that script"
+
+curl -fsSL https://ollama.com/install.sh | sh
+
+echo "Installing ArkGPT Ollama"
+
+git clone --branch ArcGPT-Ollama https://github.com/arc360alt/ArcGPT.git
+cd ArcGPT
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+clear
+
+echo "Thank you for using ArkGPT Ollama Easyinstall script!"
+echo "You can now cd into arkgpt and run the script by doing the following"
+
+echo "cd ArcGPT"
+echo "./run.sh"
---
+++ run.sh
@@ -0,0 +1,4 @@
+echo "WARNING: LINUX ONLY"
+
+source venv/bin/activate
+python ollama_gui.py
